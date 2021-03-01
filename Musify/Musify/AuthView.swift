c90e63376d56c86fecccbf0831369a4bf1a63852
zach dorame barajas <zachdoramebarajas@gmail.com>
2021-03-01 12:00:47 -0800

    authView should change based on whether we are or are not authorized

--- Musify/Musify/AuthView.swift
+++ Musify/Musify/AuthView.swift
@@ -15,29 +15,41 @@ struct AuthView: View {
     var body: some View {
         ZStack{
             Color.black
-            VStack{
-                Text("Musify")
+            if spotify.isAuthorized{
+                VStack{
+                Text("we are authorized")
                     .font(.system(size: 56, weight: .medium))
                     .foregroundColor(.white)
-                Image("spotify logo green")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 360, height: 360)
-                Text("Personalized Music for You")
-                    .font(.system(size: 25, weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button {
-                    print("auth button was clicked")
-                    self.spotify.authorize()
-                } label : {
-                    Text("Log in with Spotify")
-                        .font(.system(size: 25, weight: .medium))
-                        .foregroundColor(.green)
                 }
-                Spacer()
-                
+            }
+            else{
+                VStack{
+                    Text("Musify")
+                        .font(.system(size: 56, weight: .medium))
+                        .foregroundColor(.white)
+                    Image("spotify logo green")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 360, height: 360)
+                    Text("Personalized Music for You")
+                        .font(.system(size: 25, weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    Button {
+                        if !spotify.isAuthorized{
+                            print("we are not currently authorized")
+                        }
+                        print("auth button was clicked")
+                        self.spotify.authorize()
+
+                    } label : {
+                        Text("Log in with Spotify")
+                            .font(.system(size: 25, weight: .medium))
+                            .foregroundColor(.green)
+                    }
+                    Spacer()
+                }
             }
         }
     }
